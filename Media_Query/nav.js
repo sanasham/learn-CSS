3eddd4571169f0ee467ff73028f6d9730e27342b
sanasham <sayyedesubvali@gmail.com>
2023-10-15 01:52:19 +0530

    css added

---
+++ Media_Query/nav.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementsByClassName('.toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+console.log(toggleButton);
+console.log(navbarLinks);
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active');
+});
